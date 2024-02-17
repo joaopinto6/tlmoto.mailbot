6e0f899f08a0bc3c8ec0371fda266308b4b99971
Joao Pinto <joaomeypinto6@gmail.com>
2024-02-17 19:59:57 +0000

    try new check method

--- flask-server.py
+++ flask-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from email.message import EmailMessage
 import ssl
 import smtplib
@@ -32,11 +32,13 @@ def send_email():
 
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
-    
-    return '', 204
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+        return jsonify({'success': True}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run()
